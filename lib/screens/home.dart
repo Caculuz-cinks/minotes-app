4f678484cec84e8763437a7cda0e3f275ec110f2
Caculuz-cinks <iadeleke01@gmail.com>
2020-09-01 17:40:22 +0100

    Home screen done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,16 @@
+import 'package:MiNotes/ui/colors/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:MiNotes/services/note_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class CustomPopupMenu{
+class CustomPopupMenu {
   String label, route;
-  CustomPopupMenu({ this.label, this.route });
+  CustomPopupMenu({this.label, this.route});
 }
 
 class _HomeState extends State<Home> {
@@ -24,24 +26,30 @@ class _HomeState extends State<Home> {
   );
 
   @override
-  initState(){
+  initState() {
     super.initState();
     initNotes();
   }
 
   //create boxes for mitodo types
-  Widget todoType(type, IconData icon){
+  Widget todoType(type, IconData icon) {
     Color backgroundColor;
-    if(type == "Work") backgroundColor  = Colors.green[600];
-    else if(type == "Finance") backgroundColor  = Colors.red[600];
-    else if(type == "Travel") backgroundColor  = Colors.yellow[900];
-    else if(type == "Study") backgroundColor  = Colors.cyan;
-    else if(type == "Personal") backgroundColor  = Colors.blue;
-    else backgroundColor  = Colors.pinkAccent;
+    if (type == "Work")
+      backgroundColor = AppColors.workColor.withOpacity(0.5);
+    else if (type == "Finance")
+      backgroundColor = AppColors.financeColor.withOpacity(0.5);
+    else if (type == "Travel")
+      backgroundColor = AppColors.travelColor.withOpacity(0.5);
+    else if (type == "Study")
+      backgroundColor = AppColors.studyColor.withOpacity(0.5);
+    else if (type == "Personal")
+      backgroundColor = AppColors.personalColor.withOpacity(0.5);
+    else
+      backgroundColor = AppColors.familyColor.withOpacity(0.5);
     return Column(
       children: [
         GestureDetector(
-          onTap: (){
+          onTap: () {
             Navigator.pushNamed(context, "/newnote", arguments: {
               "color": backgroundColor.toString(),
               "type": type,
@@ -49,135 +57,177 @@ class _HomeState extends State<Home> {
             });
           },
           child: Container(
-            width: 150.0,
-            height: 150.0,
-            padding: EdgeInsets.all(10.0),
-            color: backgroundColor,
+            width: 145.0,
+            height: 160.0,
+            // padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: Center(
                 child: Wrap(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                            icon,
-                            color: Colors.white
-                        ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                            type,
-                          style: TextStyle(
-                            fontSize: 20.0
-                          ),
-                        )
-                      ],
+                    Icon(icon, color: Colors.white),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      type,
+                      style: TextStyle(fontSize: 20.0),
                     )
                   ],
                 )
-            ),
+              ],
+            )),
           ),
         )
       ],
     );
   }
 
-  List <PopupMenuEntry> choices = [PopupMenuItem(child: Text("one"), value: "One",)];
+  List<PopupMenuEntry> choices = [
+    PopupMenuItem(
+      child: Text("one"),
+      value: "One",
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("MiNotes"),
+        elevation: 0.0,
+        backgroundColor: Color(0xffffffff),
         actions: [
-          IconButton(
-            onPressed: (){
+          GestureDetector(
+            onTap: () {
               Navigator.pushNamed(context, "/profile");
-            },icon: Icon(Icons.account_circle, size: 35.0,),),
-          PopupMenuButton(itemBuilder: (context){
-            return choices.map((item) => PopupMenuItem(
-              child: Text("About"),
-              value: "About",
-              )
-            ).toList();
-          }, onSelected: (value){
-            Navigator.pushNamed(context, "/about");
-          },)
+            },
+            child: SvgPicture.asset(
+              'assets/profile.svg',
+              semanticsLabel: 'profile icon',
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 25.0),
+            child: PopupMenuButton(
+              child: SvgPicture.asset(
+                'assets/options.svg',
+                semanticsLabel: 'profile icon',
+              ),
+              itemBuilder: (context) {
+                return choices
+                    .map((item) => PopupMenuItem(
+                          child: Text("About"),
+                          value: "About",
+                        ))
+                    .toList();
+              },
+              onSelected: (value) {
+                Navigator.pushNamed(context, "/about");
+              },
+            ),
+          )
         ],
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                        "Add new from",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                   todoType("Work", Icons.work),
-                    SizedBox(width: 8.0,),
-                    todoType("Finance", Icons.credit_card),
-                  ],
-                ),
-                SizedBox(height: 8.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    todoType("Travel", Icons.airplanemode_active),
-                    SizedBox(width: 8.0,),
-                    todoType("Study", Icons.library_books),
-                  ],
-                ),
-                SizedBox(height: 8.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    todoType("Personal", Icons.person),
-                    SizedBox(width: 8.0,),
-                    todoType("Family",  Icons.people),
-                  ],
-                ),
-                SizedBox(height: 8.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FlatButton.icon(
-                        onPressed: (){
-                          Navigator.pushNamed(context, "/allnotes");
-                        },
-                      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
-                        icon: Icon(Icons.remove_red_eye, color: Colors.white,),
-                        label: Text(
-                          "View All",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 55,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'MiNotes',
+                style: TextStyle(
+                    color: AppColors.primaryColor,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Add New ",
+                    style: TextStyle(
+                        color: AppColors.primaryColor,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  todoType("Work", Icons.work),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  todoType("Finance", Icons.credit_card),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  todoType("Travel", Icons.airplanemode_active),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  todoType("Study", Icons.library_books),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  todoType("Personal", Icons.person),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  todoType("Family", Icons.people),
+                ],
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ButtonTheme(
+                    height: 55,
+                    minWidth: 172,
+                    child: FlatButton(
+                        onPressed: () {},
+                        color: AppColors.primaryColor,
+                        child: Text(
+                          'View All',
                           style: TextStyle(
                               color: Colors.white,
-                            fontSize: 20.0
-                          ),),
-                        color: Colors.indigo,
-                    )
-                  ],
-                )
-              ],
-            ),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        )),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
---
+++ lib/ui/colors/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF5B58F8);
+  // static const Color secondaryColor = Color(0xFFFE1952);
+
+  static const Color workColor = Color(0xFF1CA9F9);
+
+  static const Color financeColor = Color(0xFFFF2C2C);
+  static const Color travelColor = Color(0xFFFF8125);
+
+  static const Color studyColor = Color(0xFF222BFF);
+  static const Color personalColor = Color(0xFF38A604);
+  //Black
+  static const Color black = Color(0xFF000000);
+  static const Color familyColor = Color(0xFFDE15FF);
+
+  //White
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color whiteShade1 = Color(0xFFFFFFDE);
+
+  //Grey
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color greyShade1 = Color(0xFFE7E7E7);
+
+  //Red
+  static const Color red = Color(0xFFF1291A);
+
+  //Green
+  static const Color green = Color(0xFF3ACC6C);
+
+  //AlternateColors
+  static const Color alternateShade1 = Color(0xFFF2DC5D);
+  static const Color alternateShade2 = Color(0xFFF2A359);
+  static const Color alternateShade3 = Color(0xFFDB9065);
+  static const Color alternateShade4 = Color(0xFFA4031F);
+}
