b5ebb37791b42ac8ee943a33e9b3b2015d2f2a0d
Caculuz-cinks <iadeleke01@gmail.com>
2020-09-02 17:17:37 +0100

    UI completed

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -30,84 +30,94 @@ class About extends StatelessWidget {
             ),
             Container(
               padding: EdgeInsets.all(20.0),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset("assets/logo.svg"),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "MiNotes",
-                          style: TextStyle(
-                              color: Colors.indigo,
-                              fontSize: 40.0,
-                              fontWeight: FontWeight.w900),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "v1.0.0",
-                          style: TextStyle(color: Colors.indigo),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "by",
-                          style: TextStyle(color: Colors.indigo),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Code2geda",
-                          style: TextStyle(color: Colors.indigo),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Developers:",
-                          style: TextStyle(color: Colors.indigo),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 5.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Micah Elijah",
-                          style: TextStyle(
-                              color: Colors.indigo,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset("assets/logo.png"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "MiNotes",
+                        style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w900),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "v1.0.0",
+                        style: TextStyle(color: AppColors.primaryColor),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "by",
+                        style: TextStyle(color: AppColors.primaryColor),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Code2geda",
+                        style: TextStyle(color: AppColors.primaryColor),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Developers:",
+                        style: TextStyle(color: AppColors.primaryColor),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Micah Elijah",
+                        style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Isaiah Adeleke",
+                        style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
           ],
